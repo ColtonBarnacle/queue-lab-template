3c35ae2941f706aad4524e9897379de5b48cfb12
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-15 09:52:30 -0600

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,14 +1,14 @@
-class Queue:
+class Queue():
     def __init__(self):
         self.cards = []
 
-    #REMOVE PASS AND COMPLETE THE FUNCTION
+ #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        pass
+        self.cards.append(self, item)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        del self.cards[-1]
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
